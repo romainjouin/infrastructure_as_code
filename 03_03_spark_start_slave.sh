4b18520921113ff9b1b59048465430d05dac2bc0
romain jouin <romain.jouin@gmail.com>
2017-01-03 16:23:03 +0100

    adding spark start master.sh

--- 03_03_spark_start_slave.sh
+++ 03_03_spark_start_slave.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-url="http://d3kbcqa49mib13.cloudfront.net/spark-1.6.3-bin-hadoop2.6.tgz"
-nom_tgz=$(basename $url)
-spark_version=${nom_tgz::-4}
 slave=$1
 ip_master=$2
-echo "ssh  $slave "~/Informatique/$spark_version/sbin/start-slave.sh spark://$ip_master:7077""
-ssh  $slave "~/Informatique/$spark_version/sbin/start-slave.sh spark://$ip_master:7077"
+if [[ $# -ne 2 ]]
+then
+	echo "Il faut deux arguments : (1 = ip du slave, 2 =  ip du maître"
+else
 
+	echo "ssh  $slave "$spark_home/sbin/start-slave.sh spark://$ip_master:7077""
+	ssh  $slave "$spark_home/sbin/start-slave.sh spark://$ip_master:7077"
+fi
