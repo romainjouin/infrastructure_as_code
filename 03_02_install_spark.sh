9b7015c8a3c08877392bfcbb7ebf50e84515034c
romain <romain.jouin@gmail.com>
2017-01-25 11:27:40 +0100

    adding install sparl / download hadoop

--- 03_02_install_spark.sh
+++ 03_02_install_spark.sh
@@ -4,5 +4,5 @@ spark_uncompressed="${spark_tgz::-4}"
 mkdir -p  "$(dirname $spark_home)"
 cd        $download_dir
 #curl -O   $spark_tgz_url
-tar -xvf  $spark_tgz
+#tar -xvf  $spark_tgz
 mv $spark_uncompressed $spark_home
