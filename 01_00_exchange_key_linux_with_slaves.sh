9938a9ab7f75070080bf84130a167eaa977655f5
romain jouin <romain.jouin@gmail.com>
2018-01-16 10:12:40 +0100

    Adding the file with a loop to send ssh key to all machines in network.

---
+++ 01_00_exchange_key_linux_with_slaves.sh
@@ -0,0 +1,43 @@
+source 00_00_conf_reseau.conf
+#exchange_key.sh
+for destination  in "${ips[@]}"
+do
+	# 1) create a key
+	if [ -e ~/.ssh/id_rsa ]; then
+		echo 'Key exist, reusing.'
+	else
+		ssh-keygen -t rsa -f ~/.ssh/id_rsa
+	fi
+	# 2) send the key to the distant server
+	cat ~/.ssh/id_rsa.pub | ssh $destination "cat - >> ~/.ssh/authorized_keys"
+
+	# 3) create an agent on your bashrc
+	echo '
+		SSH_ENV="$HOME/.ssh/environment"
+		function start_agent {
+		     echo "Initialising new SSH agent..."
+		     /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+		     echo succeeded
+		     chmod 600 "${SSH_ENV}"
+		     . "${SSH_ENV}" > /dev/null
+		     /usr/bin/ssh-add;
+		}
+		# Source SSH settings, if applicable
+
+		if [ -f "${SSH_ENV}" ]; then
+		     . "${SSH_ENV}" > /dev/null
+		     #ps ${SSH_AGENT_PID} doesnt work under cywgin
+		     ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+		         start_agent;
+		     }
+		else
+		     start_agent;
+		fi
+		' >> ~/.bashrc
+	# 4) activate the agent
+	source ~/.bashrc
+	# 5) tell the agent wich secret he should know to become a secret agent.
+	ssh-add ~/.ssh/id_rsa
+	# 6) enjoy a free connection to the distant host
+#	ssh $destination
+done
