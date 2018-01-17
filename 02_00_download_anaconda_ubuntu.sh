7e857ba9591da79ed410360af8a5164252ee381c
romainjouin <romain.jouin@gmail.com>
2018-01-17 19:53:00 +0100

    Update 02_00_download_anaconda_ubuntu.sh

--- 02_00_download_anaconda_ubuntu.sh
+++ 02_00_download_anaconda_ubuntu.sh
@@ -1,8 +1,3 @@
-source 00_00_useful_functions_generic.sh
-url=https://repo.continuum.io/archive/Anaconda2-4.2.0-Linux-x86_64.sh
-url=https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86.sh
-filename="$(basename $url)"
-target_path="$base_dir/$filename"
-download $url
-bash target_path
-
+curl -O https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86.sh
+chmod Anaconda3-5.0.1-Linux-x86.sh
+./Anaconda3-5.0.1-Linux-x86.sh
