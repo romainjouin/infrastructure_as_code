7bbb3dcea218cf5ca09b671ed06380f7e35f5014
romain jouin <romain.jouin@gmail.com>
2018-01-17 17:04:17 +0100

    updating.

--- 02_00_download_anaconda_ubuntu.sh
+++ 02_00_download_anaconda_ubuntu.sh
@@ -1,7 +1,6 @@
-#!bin/bash
 source 00_00_useful_functions_generic.sh
 url=https://repo.continuum.io/archive/Anaconda2-4.2.0-Linux-x86_64.sh
-
+url=https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86.sh
 filename="$(basename $url)"
 target_path="$base_dir/$filename"
 download $url
--- 03_03_start_spark_on_jupyter.sh
+++ 03_03_start_spark_on_jupyter.sh
@@ -1,2 +1,8 @@
+source 00_00_conf_reseau.conf
 cd ~
-IPYTHON_OPTS="notebook --no-browser --ip=*"  ~/spark/spark-1.6/bin/pyspark
+export PYSPARK_PYTHON=/home/spark/anaconda3/bin/python3.6
+export PYTHONHASHSEED=323
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook --ip=* --no-browser"
+export PYSPARK_DRIVER_PYTHON=jupyter 
+~/spark/bin/pyspark –-master spark://$master:7077 --total-executor-cores 2
+
