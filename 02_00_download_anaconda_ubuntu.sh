770018b4e32e0a7a8a52c9a6816abe8fdb982369
romainjouin <romain.jouin@gmail.com>
2018-01-17 19:59:24 +0100

    Update 02_00_download_anaconda_ubuntu.sh

--- 02_00_download_anaconda_ubuntu.sh
+++ 02_00_download_anaconda_ubuntu.sh
@@ -1,3 +1,7 @@
+if [ -e '~/python3/' ] ; then
+  rm -rf ~/python3/
+fi
+
 curl -O https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
 chmod +x Anaconda3-5.0.1-Linux-x86_64.sh
-./Anaconda3-5.0.1-Linux-x86.sh
+./Anaconda3-5.0.1-Linux-x86_64.sh
