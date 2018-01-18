4383671bb9748d556b98075b170f0386011fd588
romain jouin <romain.jouin@gmail.com>
2018-01-18 11:53:54 +0100

    working !

--- 02_00_download_anaconda_ubuntu.sh
+++ 02_00_download_anaconda_ubuntu.sh
@@ -1,7 +1,14 @@
-if [ -d '~/python3/' ] ; then
-	echo "python3 seems to be already installed"
+if [ -d ~/anaconda3/z ] ; then
+	echo "anaconda3 seems to be already installed"
 else
-	curl -O https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
-	chmod +x Anaconda3-5.0.1-Linux-x86_64.sh
-	./Anaconda3-5.0.1-Linux-x86_64.sh -b
+echo "doesn't exist"
+url=https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
+delimiter="\/"
+echo "going to install $url"
+anaconda=$(echo $url | sed -e "s/$delimiter/\n/g" | tail -1)
+echo $anaconda
+	curl -O $url
+
+	chmod +x $anaconda
+	./$anaconda -b
 fi
