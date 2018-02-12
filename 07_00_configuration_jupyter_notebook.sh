4a992563df5c20f688a41e43149c887c13ebb3da
romainjouin <romain.jouin@gmail.com>
2018-02-12 15:09:18 +0100

    Update 07_00_configuration_jupyter_notebook.sh

--- 07_00_configuration_jupyter_notebook.sh
+++ 07_00_configuration_jupyter_notebook.sh
@@ -1,4 +1,4 @@
 echo "export PYSPARK_PYTHON=/home/spark/anaconda3/bin/python3.6" >> ~/.bashrc
 echo "export PYTHONHASHSEED=323" >> ~/.bashrc
-echo "export PYSPARK_DRIVER_PYTHON_OPTS="notebook --ip=* "" >> ~/.bashrc
+echo "export PYSPARK_DRIVER_PYTHON_OPTS='notebook --ip=* '" >> ~/.bashrc
 echo "export PYSPARK_DRIVER_PYTHON=jupyter " >> ~/.bashrc
